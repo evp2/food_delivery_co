1a6720c7e2cd6e551e984a5d131969c38a70eb0c
Evan Pomponio <32994398+epomp447@users.noreply.github.com>
2019-11-15 16:25:26 -0500

    Created Pronto Pizza and panda express scripts
    also updated the entry in pickRestaurants.php

--- pickRestaurant.php
+++ pickRestaurant.php
@@ -4,7 +4,7 @@
     <title> Choose Restaurant </title>
 </head>
 <body>
-<form action="http://localhost:1234/addordergrandchina.php" method="post">
+<form action="http://192.168.64.2/addordergrandchina.php" method="post">
 
     <b>Choose A Restaurant </b>
 
@@ -14,47 +14,47 @@
 
 </form>
 
-<form action="http:://localhost:1234/pandaexpress.php" method = "post">
+<form action="http://192.168.64.2/pandaexpress.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Panda Express" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/prontopizza.php" method = "post">
+<form action="http://192.168.64.2/prontopizza.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Pronto Pizza" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/yellowsubmarine.php" method = "post">
+<form action="http://localhost:1234/yellowsubmarine.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Yellow Submarine" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/charleys.php" method = "post">
+<form action="http://localhost:1234/charleys.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Charleys Philly Steaks" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/unos.php" method = "post">
+<form action="http://localhost:1234/unos.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Uno's Pizzeria and Grill" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/oldsanjuan.php" method = "post">
+<form action="http://localhost:1234/oldsanjuan.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Old San Juan" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/fiveguys.php" method = "post">
+<form action="http://localhost:1234/fiveguys.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Five Guys" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/lacocina.php" method = "post">
+<form action="http://localhost:1234/lacocina.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="La Concina" size = "40" /> </p>
 </form>
 
-<form action="http:://localhost:1234/palaceofasia.php" method = "post">
+<form action="http://localhost:1234/palaceofasia.php" method = "post">
 
     <p> <input type = "submit" name= "submit" value="Palace of Asia" size = "40" /> </p>
 </form>
