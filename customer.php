72e5b9ec21fe38fbef974c341baa7a5a38e7a066
Evan Pomponio <32994398+epomp447@users.noreply.github.com>
2019-11-17 16:03:03 -0500

    Add files via upload

--- customer.php
+++ customer.php
@@ -4,108 +4,74 @@
 </head>
 <body>
 
-
 <?php
-
-
-
 if(isset($_POST['submit'])){
-
     $data_missing = array();
 //Feel free to add any other attributes needed : address, location or if we need a login screen
-
     if(empty($_POST['firstName'])){
-
         // Adds name to array
         $data_missing[] = 'First Name';
-
     } else {
-
         // Trim white space from the name and store the name
         $f_name = trim($_POST['firstName']);
-
     }
-
     if(empty($_POST['lastName'])){
-
         // Adds name to array
         $data_missing[] = 'Last Name';
-
     } else {
-
         // Trim white space from the name and store the name
         $l_name = trim($_POST['lastName']);
-
     }
-
     if(empty($_POST['phoneNumber'])){
-
         // Adds name to array
         $data_missing[] = 'Phone Number';
-
     } else {
-
         // Trim white space from the name and store the name
         $phone = trim($_POST['phoneNumber']);
-
     }
-
-
-    if(empty($data_missing)){
-
-        require_once('../mysqli_connect.php');
-
-        //inserting query : make sure variables match yours
-        $query = "INSERT INTO customers (customerid, address, firstName,
-        lastName, email, phoneNumber) VALUES (NULL, NULL, ?, ?, NULL, ?)";
-
-
-        $stmt = mysqli_prepare($dbc1, $query);//just needs to adjust to $dbc
-
-
-
-        mysqli_stmt_bind_param($stmt, "ssi", $f_name, $l_name, $phone);
-
-        mysqli_stmt_execute($stmt);
-
-        $affected_rows = mysqli_stmt_affected_rows($stmt);
-
-        if($affected_rows == 1){
-
-            echo 'Name Added!';
-
-            mysqli_stmt_close($stmt);
-
-            mysqli_close($dbc1);//same here adjust
-
-        } else {
-
-            echo 'Error Occurred<br />';
-            echo mysqli_error($dbc1);//adjust dbc
-
-            mysqli_stmt_close($stmt);
-
-            mysqli_close($dbc1);//adjust dbc
-
-        }
-
+    if(empty($_POST['address'])){
+        // Adds name to array
+        $data_missing[] = 'Address';
     } else {
-
-        echo 'You need to enter the following data<br />';
-
-        foreach($data_missing as $missing){
-
-            echo "$missing<br />";
-
-        }
-
+        // Trim white space from the name and store the name
+        $address = trim($_POST['address']);
     }
-
+    if(empty($_POST['email'])){
+        // Adds name to array
+        $data_missing[] = 'Email';
+    } else {
+        // Trim white space from the name and store the name
+        $email = trim($_POST['email']);
+    }
+    if(empty($data_missing)){
+        require_once('./connect.php');
+        //inserting query : make sure variables match yours
+        $query = "INSERT INTO customers(customerid, address, firstName,
+        lastName, email, phoneNumber) VALUES (NULL, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($dbc, $query);//just needs to adjust to $dbc
+        mysqli_stmt_bind_param($stmt, "ssssi", $address, $f_name, $l_name, $email, $phone);
+        mysqli_stmt_execute($stmt);
+        $affected_rows = mysqli_stmt_affected_rows($stmt);
+        if($affected_rows == 1){
+            echo 'Name Added!';
+            mysqli_stmt_close($stmt);
+            mysqli_close($dbc);//same here adjust
+        } else {
+            echo 'Error Occurred<br />';
+            echo mysqli_error($dbc);//adjust dbc
+            mysqli_stmt_close($stmt);
+            mysqli_close($dbc);//adjust dbc
+        }
+    } else {
+        echo 'You need to enter the following data<br />';
+        foreach($data_missing as $missing){
+            echo "$missing<br />";
+        }
+    }
 }
-
 ?>
 
-<form action = "http://localhost:1234/customer.php" method = "post">
+<form action = "http://192.168.64.2/customer.php" method = "post">
 
     <b> Please enter a name and number for your order: </b>
     <p> First Name: <input type = "text" name = "firstName" value = " "/> </p>
@@ -114,13 +80,15 @@ if(isset($_POST['submit'])){
 
     <p> Phone Number: <input type = "text" name = "phoneNumber" value = " " /> </p>
 
-    <p> <input type = "submit" name = "submit" value = "Enter"/> </p>
+    <p> Address: <input type = "text" name = "address" value = " " /></p>
 
-    <p>                  </p>
+    <p> Email: <input type = "text" name = "email" value = " " /></p>
+
+    <p> <input type = "submit" name = "submit" value = "Enter"/> </p>
 
 </form>
 
-<form action="http://localhost:1234/pickuporder.php" method="post">
+<form action="http://192.168.64.2/pickuporder.php" method="post">
 
 
     <b>Would you like Pick Up or Delivery? </b>
@@ -131,19 +99,10 @@ if(isset($_POST['submit'])){
 
 </form>
 
-<form action="http://localhost:1234/delivery.php" method = "post">
+<form action="http://192.168.64.2/delivery.php" method = "post">
 
     <p> <input type = "submit" name= "service" value="DELIVERY" size = "40" /> </p>
+
 </form>
-
-
-
-
-
 </body>
 </html>
-
-
-
-
-
