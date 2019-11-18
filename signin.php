fdee9ba7578e4f1a5e2ed4581dece060d828b7a0
ChristinaJDiaz <cd663587@sju.edu>
2019-11-18 10:35:10 -0500

    Add files via upload

--- signin.php
+++ signin.php
@@ -1,30 +1,39 @@
-<html>
+
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title> Choose Service </title>
+    <title>Sign in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<form action = "http://localhost:1234/customer.php" method = "post">
-
-<b> Please enter your information to begin placing an order: </b>
-<p> First Name: <input type = "text" name = "firstName" value = " "/> </p>
-
-<p> Last Name:  <input type = "text" name = "lastName" value = " "/> </p>
-
-    <p> Phone Number: <input type = "text" name = "phoneNumber" value = " " /> </p>
-
-
-
-    <p> <input type = "submit" name = "submit" value = "Enter"/> </p>
-
-<p>                  </p>
-
-</form>
-
-
-
-
-
+<div class="container">
+    <h2>Order Now</h2>
+    <p> Please enter the following before placing your order</p>
+    <form action="http://localhost:1234/customer.php">
+        <div class="form-group">
+            <label for="firstName">First Name:</label>
+            <input type="firstName" class="form-control" id="firstName" placeholder="Enter First Name" name="firstName">
+        </div>
+        <div class="form-group">
+            <label for="lastName">Last Name:</label>
+            <input type="lastName" class="form-control" id="lastName" placeholder="Enter Last Name" name="lastName">
+        </div>
+        <div class="form-group">
+            <label for="phoneNumber">Phone Number:</label>
+            <input type="phoneNumber" class="form-control" id="phoneNumber" placeholder="Enter Phone Number" name="phoneNumber">
+        </div>
+        <button type="submit" name = "submit" class="btn btn-default">Enter</button>
+    </form>
+</div>
 
 </body>
 </html>
